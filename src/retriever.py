25bc451371ba536c0f3b3ad0d324ad013d829298
steeltroops11 <meenalsharm982@gmail.com>
2026-09-20 20:38:11 +0530

    Build Chroma collection when missing

--- src/retriever.py
+++ src/retriever.py
@@ -17,11 +17,32 @@ class CropKnowledgeRetriever:
         self.client = chromadb.PersistentClient(path=str(VECTOR_DB_DIR))
         self.embed_fn = get_embedding_function()
 
+	HEAD
         self.collection = self.client.get_or_create_collection(
             name=CHROMA_COLLECTION_NAME,
             embedding_function=self.embed_fn,
         )
 
+        try:
+            self.collection = self.client.get_collection(
+                name=CHROMA_COLLECTION_NAME,
+                embedding_function=self.embed_fn,
+            )
+        except Exception:
+            from src.ingest import ingest_knowledge_base
+	    ce3609f (Build Chroma collection when missing)
+
+            print(
+                f"Collection '{CHROMA_COLLECTION_NAME}' not found. "
+                "Building ChromaDB from the knowledge base..."
+            )
+
+            ingest_knowledge_base()
+
+            self.collection = self.client.get_collection(
+                name=CHROMA_COLLECTION_NAME,
+                embedding_function=self.embed_fn,
+            )
     def retrieve(
         self,
         query: str,
